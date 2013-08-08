7205e23495fd185167af8a3da42a4a0009a398b5
Kyle Park <wha754@gmail.com>
2013-08-08 18:25:02 -0400

    without local settings

--- optme/settings.py
+++ optme/settings.py
@@ -110,8 +110,8 @@ ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
-    'compressor.finders.CompressorFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    #'compressor.finders.CompressorFinder',
 )
 
 STATICFILES_DIRS = (
